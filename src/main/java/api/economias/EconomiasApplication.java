324d71c3830162f61d41087a86bb82f7f0182395
Victor Ruan Fabricio Cavalcante <victor.cavalcante@matriz.fornecedoramaquinas.com.br>
2025-06-12 12:24:05 -0300

    começando api

---
+++ src/main/java/api/economias/EconomiasApplication.java
@@ -0,0 +1,13 @@
+package api.economias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EconomiasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/api/economias/EconomiasApplicationTests.java
@@ -0,0 +1,13 @@
+package api.economias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomiasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
