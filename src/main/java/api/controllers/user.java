e1d1dbac9dba786b21ffa149265405b4672733a9
Victor Ruan Fabricio Cavalcante <victor.cavalcante@matriz.fornecedoramaquinas.com.br>
2025-06-12 12:54:28 -0300

    ajeitando web

--- src/main/java/api/controllers/user.java
+++ src/main/java/api/controllers/user.java
@@ -1,9 +1,11 @@
 package api.controllers;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
+@RequestMapping
 public class user {
     
 }
